f294d7187b966e806f263bd533792379fc7982d5
Pauliina Granström <amk1006359@student.hamk.fi>
2024-10-16 21:42:47 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,61 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+int ika = 18;
+
+// Tulostusehdot
+
+// if (ika >= 0 && ika < 18)
+
+// {
+
+// System.out.println("Olet alaikäinen");
+
+// }
+
+// else
+
+// {
+
+// System.out.println("Olet aikuinen");
+
+// }
+
+// if (ika > 0 && ika < 18) 
+// { 
+//     System.out.println("Olet alaikäinen"); 
+// }
+//  else if (ika >= 65) 
+// {
+//     System.out.println("Olet eläkeläinen"); 
+// }
+//  else { System.out.println("Olet aikuinen"); 
+// }
+if (ika > 0 && ika < 18) 
+{
+    System.out.println("Olet alaikäinen");
+    
+    if (ika >= 15) {
+    
+    System.out.println("Saat ajaa mopoa"); 
+    }
+
+    if (ika >= 16) {
+
+    System.out.println("Saat ajaa kevaria"); 
+    }
+    
+        if(ika == 10 || ika == 20 || ika == 20 || ika == 30 || ika == 40 || ika == 50 || ika == 60 || ika == 70 || ika == 80 || ika == 90) {
+        System.out.println("Onnea tasavuosista"); }
+    
+    }else if (ika >= 65) {
+    
+    System.out.println("Olet eläkeläinen");
+     
+    } else {
+    
+    System.out.println("Olet aikuinen");
+    
+    } 
+    }
+}
